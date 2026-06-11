75ba26b3490b54c41294e3b6a71e9361b9423968
DdavidV <themani@hotmail.hu>
2026-06-11 22:00:41 +0200

    init: chaos begins here

---
+++ lib/openapi.ex
@@ -0,0 +1,3 @@
+defmodule Openapi do
+
+end
---
+++ mix.exs
@@ -0,0 +1,25 @@
+defmodule Openapi.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :openapi,
+      version: "0.1.0",
+      elixir: "~> 1.19",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {Openapi.Application, []}
+    ]
+  end
+
+  defp deps do
+    [
+    ]
+  end
+end
